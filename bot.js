9a6e69252f5ba31b964639185475f77120bfdb5f
A7mdKW <43846712+A7mdKW@users.noreply.github.com>
2018-12-16 21:38:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,6 @@ const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const sql = require("sqlite");
  const dateFormat = require('dateformat'); 
  const pretty = require('pretty-ms') 
- const prefix = "-";
 client.on('ready', () => {
     console.log('I am ready!');
 });
