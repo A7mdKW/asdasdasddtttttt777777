2c0b37afc8b8ce7ca3d450d814df5e13ab481036
A7mdKW <43846712+A7mdKW@users.noreply.github.com>
2018-12-16 21:47:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,21 +31,6 @@ client.on('ready', () => {
 
 });
 
-client.on("message", message => {
-
-            if (message.content.startsWith(prefix + "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
-});
-
-
 client.on('message', message => {
   if (!message.content.startsWith(PREFIX)) return;
   var args = message.content.split(' ').slice(1);
